a5434c5570a5e00eeb6c8a43c178fca42bdebeb3
ZheN093 <grice850301@gmail.com>
2026-02-02 21:45:46 +0800

    Update 宅宅行長的大別墅.py

--- 宅宅行長的大別墅.py
+++ 宅宅行長的大別墅.py
@@ -4,4 +4,4 @@ import time
 
 st.set_page_config(page_title="宅宅行長的大別墅", layout="centered")
 st.title("歡迎來到宅宅行長的大別墅！")
-st.write("這是一個展示宅宅行長豪華別墅的應用程式。請稍等，正在加載內容...")
+st.markdown("""宅宅行長的大別墅是個超級棒的地方！""")
