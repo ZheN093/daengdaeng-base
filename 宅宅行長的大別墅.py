fb0bf02d9833deb6d28fa06f58e3535f81e5eec4
ZheN093 <grice850301@gmail.com>
2026-02-02 22:08:44 +0800

    Update 宅宅行長的大別墅.py

--- 宅宅行長的大別墅.py
+++ 宅宅行長的大別墅.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import time
 
-st.set_page_config(page_title="宅宅行長的大別墅", 
-                   layout="centered"
-                   page_icon=":house:")
+st.set_page_config(page_title="宅宅行長的大別墅"
+                   .layout="centered"`,page_icon=":house:")
 
 st.title(" :green[歡迎來到宅宅行長的大別墅！]")
 st.markdown("""這是宅宅行長的大別墅 :house:，宅宅行長家甚麼有喔!
@@ -14,5 +13,6 @@ if st.button("我要參觀一下"):
         time.sleep(1.5)
     st.balloons()
     st.success("歡迎光臨！")
+
 
 
